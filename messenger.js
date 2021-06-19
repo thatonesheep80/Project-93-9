c0848989c1e9df603ff7fc0299264bd1b4e8bcbd
thatonesheep80 <79360988+thatonesheep80@users.noreply.github.com>
2021-06-19 16:36:24 +0530

    Add files via upload

---
+++ messenger.js
@@ -0,0 +1,8 @@
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem ("Username" , user_name);
+
+    window.location = "messenger_room.html";
+}
---
+++ messenger_room.js
@@ -0,0 +1,61 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB5-ShdzpTAWwF_0tXTqyZF7tjHVDTbu_s",
+    authDomain: "kwitter-a83bf.firebaseapp.com",
+    databaseURL: "https://kwitter-a83bf-default-rtdb.firebaseio.com",
+    projectId: "kwitter-a83bf",
+    storageBucket: "kwitter-a83bf.appspot.com",
+    messagingSenderId: "31248137496",
+    appId: "1:31248137496:web:f1aed82f200d72e189f26d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  function addUser()
+  {
+      user_name = document.getElementById("user_name").value;
+  
+      firebase.database().ref("/").child(user_name).update({
+          purpose : "adding user"
+       });
+  
+      localStorage.setItem ("Username" , user_name);
+  
+       window.location = "messenger_room.html";
+  }
+  
+  user_name = localStorage.getItem("Username");
+      document.getElementById("user_name_welcome").innerHTML = "Welcome " + user_name + "!";
+  
+      function addRoom()
+      {
+        room_name = document.getElementById("room_name").value;
+        firebase.database().ref("/").child(room_name).update({ purpose : "adding room name" });
+        localStorage.setItem("room_name" , room_name);
+        window.location = "messenger_page.html";
+      }
+  
+  function getData() 
+  {
+        firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+         Room_names = childKey;
+        //Start code
+        console.log("room_name - " + Room_names);
+        row = "<div class='room_name' id=" + Room_names + "onclick = 'redirectToRoomName(this.id)'> #" + Room_names + "</div> <hr>";
+        document.getElementById("output").innerHTML += row;
+        //End code
+        });});}
+  
+  getData();
+  
+  function redirectToRoomName(name)
+  {
+        console.log(name);
+        localStorage.setItem("room_name" , name);
+        window.location = "messenger_page.html";
+  }
+  
+
+function logout()
+{
+    window.location = "index.html";
+}
